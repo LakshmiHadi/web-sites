98ee34f4cf8027d67b601452fe7856ac8b0cf4b9
Lakshmi Hadi <149565944+LakshmiHadi@users.noreply.github.com>
2024-03-01 20:03:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelectorAll('.control_prev');
+const next_btn = document.querySelectorAll('.control_next');
+
+let n = 0;

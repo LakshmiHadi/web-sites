99c6d6df09a316aecc0bb5395ff7e1debf7189e7
Lakshmi Hadi <149565944+LakshmiHadi@users.noreply.github.com>
2024-03-02 10:56:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ const prev_btn = document.querySelectorAll('.control_prev');
 const next_btn = document.querySelectorAll('.control_next');
 
 let n = 0;
+
+function changeSlide(){
+    for(let i = 0; i < imgs.length; i++) {
+        imgs[i].computedStyleMap.display = 'none';
+    }
+    imgs[n].computedStyleMap.display = 'block';
+}
+changeSlide();
+
+prev_btn.addEventListener('click', ()=>{
+    if(n > 0){
+        n--;
+    }else{
+        n = imgs.length - 1;
+    }
+    changeSlide();
+})
+next_btn.addEventListener('click', ()=>{
+    if(n < imgs.length - 1){
+        n++;
+    }else{
+        n = 0;
+    }
+    changeSlide();
+})
